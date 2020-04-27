96ba1a8edf163f8bed5740d4ace39176213a0b97
erdemce <erdemce2234@gmail.com>
2020-04-27 16:36:11 +0200

    Only in order to try the use of Github with Anroid Studio

---
+++ app/src/main/java/erdem/android/aegerdichnicht/MainActivity.java
@@ -0,0 +1,17 @@
+package erdem.android.aegerdichnicht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* Only in order to try the use of Github with Anroid Studio
+
+         */
+    }
+}
